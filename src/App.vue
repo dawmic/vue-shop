747b8eeb9bcc834de69828b978d7a01c817eb13d
Dawid M <dawmic19@gmail.com>
2021-11-18 16:12:03 +0100

    add mock data and simulate connect to API using axios

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container-fluid bg-primary">
+      <div class="covid-info row align-items-center">
+        <p class="col-12 fs-6 text-light">Safe Covid-19 delivery</p>
+      </div>
     </div>
-    <router-view />
+    <Header />
+    <Slider />
+    <router-view :products="products" />
   </div>
 </template>
 <script>
+import Header from "./components/Header.vue";
+import Slider from "./components/Slider.vue";
+import axios from "axios";
 export default {
   name: "App",
-  components: {},
-  data(){
-    return{
-
-    }
+  components: { Header, Slider },
+  data() {
+    return {
+      filter: "",
+      products: [],
+    };
+  },
+  computed: {
+    filteredAndSortedTracks() {
+      return this.playlistInfo.tracks.filter((track) => {
+        return (
+          track.name.toLowerCase().includes(this.filter_tracks.toLowerCase()) ||
+          track.artists_names
+            .toLowerCase()
+            .includes(this.filter_tracks.toLowerCase()) ||
+          track.album_name
+            .toLowerCase()
+            .includes(this.filter_tracks.toLowerCase())
+        );
+      });
+    },
+  },
+  created() {
+    axios.get("/products.json").then((res) => {
+      console.log(res.data.products);
+      this.products = res.data.products;
+    });
   },
 };
 </script>
@@ -25,6 +53,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  .covid-info {
+    height: 50px;
+    p {
+      height: auto;
+      margin: 0;
+      padding: 0;
+    }
+  }
 }
 
 #nav {
