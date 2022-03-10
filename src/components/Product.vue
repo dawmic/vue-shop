6faf06d2d48b130584dbb60194de84b3124cd87d
dawmic <dawmic19@gmail.com>
2022-03-10 21:40:04 +0100

    add description and alignment of elements

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="product card-body mx-lg-3 mb-lg-4">
-    <h2 class="p-title card-title">{{ product.title }}</h2>
-    <img :src="product.image" alt="" />
-    <p class="p-price">{{ product.price }}</p>
-    <p class="p-rate">
+  <div class="product-container card-body mx-lg-3 mb-lg-4">
+    <h2 class="p-title card-title mt-3 ml-3">{{ product.title }}</h2>
+    <p class="p-description ml-3">{{ product.description }}</p>
+    <img class="p-image" :src="product.image" alt="" />
+    <p class="p-price mr-3">{{ product.price }}</p>
+    <p class="p-rate mr-3 mb-3">
       Rate
       <span
         v-for="n in 5"
@@ -31,15 +32,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.product {
+.product-container {
   height: 18rem;
   min-width: 10rem;
   width: 10rem;
   margin: 0.5rem;
+  padding: 0;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  justify-content: flex-start;
+  justify-content: space-between;
   border: 1px solid lightgray;
   background-color: #ffffff;
   transition: 0.2s;
@@ -53,12 +55,33 @@ export default {
   .p-description {
     color: #063757;
     font-size: 1rem;
+    text-align: left;
+    margin: 0;
+   
+  }
+  .p-description {
+    width: 90%;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    font-weight: 700;
+    line-height: 1.35;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
   }
   .p-price,
   .p-rate {
     color: #063757;
     font-size: 1.12rem;
     font-weight: 700;
+    align-self: flex-end;
+    text-align: right;
+    margin: 0;
+  }
+  .p-image {
+    width: 80%;
+    align-self: center;
+    object-fit: contain;
   }
 }
 
@@ -66,32 +89,36 @@ export default {
   color: orange;
 }
 @include media-md {
-  .product {
+  .product-container {
     height: 26rem;
     min-width: 14.9rem;
     width: 14.9rem;
+    .p-description {
+      -webkit-line-clamp: 3;
+    }
     .p-price {
       font-size: 1.3rem;
     }
   }
 }
 @include media-lg {
-  .product {
+  .product-container {
     height: 24.75rem;
     min-width: 13.8rem;
     width: 13.8rem;
+    .p-image {
+      width: 70%;
+    }
     .p-price {
       font-size: 1.5rem;
     }
   }
 }
 @include media-xl {
-  .product {
+  .product-container {
     height: 26.75rem;
     min-width: 21.3rem;
     width: 21.3rem;
   }
 }
-
-
 </style>
